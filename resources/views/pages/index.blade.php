12582e290983a4dfd357bc9e9dceb12cc0cc3604
ifeanyi Opara <itzfelz.16@gmail.com>
2021-07-09 09:43:46 +0100

    Latest Update

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,7 +4,7 @@
     <div class="jumbotron text-center">
         <h1>{{$title}}</h1>
         <p>This is my social media application for advertising</p>
-        <p><a href="/login" class="btn btn-primary btn-lg" role="button">Login</a> <a href="/register" class="btn btn-success btn-lg" role="button">Register</a></p>
+        <!-- <p><a href="/login" class="btn btn-primary btn-lg" role="button">Login</a> <a href="/register" class="btn btn-success btn-lg" role="button">Register</a></p> -->
     </div>
 
 @endsection
